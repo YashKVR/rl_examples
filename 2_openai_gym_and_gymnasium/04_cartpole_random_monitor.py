3c02b2bd3a95eb7600060c3d15b5c2a4034799df
yashkvr <yashincrypto5@gmail.com>
2026-03-14 18:09:24 +0530

    Add UV

--- 2_openai_gym_and_gymnasium/04_cartpole_random_monitor.py
+++ 2_openai_gym_and_gymnasium/04_cartpole_random_monitor.py
@@ -3,8 +3,8 @@ import gymnasium as gym
 
 if __name__ == "__main__":
     env = gym.make("CartPole-v1", render_mode="rgb_array")
-    # env = gym.wrappers.HumanRendering(env)
-    env = gym.wrappers.RecordVideo(env, video_folder="video")
+    env = gym.wrappers.HumanRendering(env)
+    # env = gym.wrappers.RecordVideo(env, video_folder="video")
 
     total_reward = 0.0
     total_steps = 0
@@ -18,5 +18,6 @@ if __name__ == "__main__":
         if done:
             break
 
-    print(f"Episode done in {total_steps} steps, total reward {total_reward:.2f}")
+    print(
+        f"Episode done in {total_steps} steps, total reward {total_reward:.2f}")
     env.close()
